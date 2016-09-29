f545a20760a861828fbb577fd6950d295feffb41
zhuanyenan <280145668@qq.com>
2016-09-29 15:54:25 +0800

    foo

--- resty/datetime.lua
+++ resty/datetime.lua
@@ -60,9 +60,9 @@ end
 local function asctime(t)
     local buf = Buf(26)
     ffi.C.asctime_r(TimeStruct(
-        t.sec, 
-        t.min, 
-        t.hour,
+        t.sec or 0, 
+        t.min or 0, 
+        t.hour or 0,
         t.day, 
         t.month - 1, 
         t.year - 1900), buf)
@@ -209,13 +209,10 @@ end
 local function test(...)
     local a = datetime.new(os.time())
     local b = datetime.new(os.date('*t')) 
-    local e = timedelta:new{day=28}
-    print(a)
-    print(a+e)
-    print(a-e)
-    print(b)
-    print(b+e)
-    print(b-e)
+    local c = a + timedelta:new{day=3}
+    local d = b - timedelta:new{day=4}
+    print('3 days after  '..tostring(a)..' is '..tostring(c))
+    print('4 days before '..tostring(b)..' is '..tostring(d))
 
     local n= 0
     local s= '1970-01-01 00:00:00'
@@ -224,10 +221,10 @@ local function test(...)
        local r = datetime.new(v)
        print(r, r.number)
     end
-    print(datetime.new('1970-1-01 0:02:00')>datetime.new(s))
+    print(datetime.new('1969-1-01 0:02:00')>datetime.new(s))
     print(datetime.new('1970-1-1 3:2:0')>datetime.new(s))
 end
-test()
+--test()
 return {
     datetime=datetime,
     timedelta=timedelta,
